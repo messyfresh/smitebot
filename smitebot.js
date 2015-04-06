68ab1d9e7d4d093d38e8f941b5154ab0bf74def2
Scott Nix <messyfresh@gmail.com>
2015-04-05 20:20:19 -0500

    Added !matchinfo to list of commands, still needs tweaked.

--- smitebot.js
+++ smitebot.js
@@ -9,16 +9,19 @@ var irc = require('irc'),
     twitchPass = process.env.twitchPass;
 
 var client = new irc.Client('irc.twitch.tv', twitchUser, {
-    //debug: true,
+    debug: true,
     userName: twitchUser,
     password: twitchPass,
     channels: [ircChannel]
 });
 
+var errorMsg = 'Sorry, an error has occurred, EVERYONE JUST STAY CALM!!!';
+
 client.addListener('message', function(from, to, message) {
     var msgArray = message.split(" ");
     if (message.toLowerCase() == "hi elo bot") {
-        client.say(ircChannel, "Hello Human Bot" + from);
+        client.say(ircChannel, "Hello Human Bot " + from);
+        client.say(ircChannel, "2Hello Human Bot " + from);
     }
     if (msgArray[0] == '!elo') {
         request({
@@ -26,7 +29,7 @@ client.addListener('message', function(from, to, message) {
             }, function (error, response, body) {
                 var jsonBody = JSON.parse(body);
                 if (error) {
-                    client.say(ircChannel, 'Sorry, an error has occurred, EVERYONE JUST STAY CALM!!!');
+                    client.say(ircChannel, errorMsg);
                 }
                 if (jsonBody.length == 0) {
                     client.say(ircChannel, 'No Results found for ' + msgArray[1]);
@@ -44,7 +47,7 @@ client.addListener('message', function(from, to, message) {
             }, function (error, response, body) {
                 var jsonBody = JSON.parse(body);
                 if (error) {
-                    client.say(ircChannel, 'Sorry, an error has occurred, EVERYONE JUST STAY CALM!!!');
+                    client.say(ircChannel, errorMsg);
                 }
                 if (jsonBody.length == 0) {
                     client.say(ircChannel, 'No Results found for ' + msgArray[1]);
@@ -66,7 +69,7 @@ client.addListener('message', function(from, to, message) {
         }, function (error, response, body) {
             var jsonBody = JSON.parse(body);
             if (error) {
-                client.say(ircChannel, 'Sorry, an error has occurred, EVERYONE JUST STAY CALM!!!');
+                client.say(ircChannel, errorMsg);
                 console.log(error);
             } if (jsonBody.length == 0) {
                 client.say(ircChannel, 'No Results found for ' + msgArray[1]);
@@ -78,6 +81,53 @@ client.addListener('message', function(from, to, message) {
                  else {
                  client.say(ircChannel, msgArray[1] + " has mastered " + masteryLevel + " gods.");
                  }
+            }
+        });
+    }
+
+    //TODO: implement better error handling for players with stupid characters in their name
+    if (msgArray[0] == '!matchinfo') {
+        request({
+            url: 'http://localhost:5000/api/getplayerstatus/' + msgArray[1]
+        }, function (error, response, body) {
+            var jsonBody = JSON.parse(body);
+            if (error) {
+                client.say(ircChannel, errorMsg);
+                console.log(error);
+            } if (jsonBody[0].status == '5') {
+                client.say(ircChannel, 'No Results found for ' + msgArray[1]);
+            } if (jsonBody[0].status == 2 || jsonBody[0].status == 1){
+                client.say(ircChannel, msgArray[1] + ' is not currently in a game.')
+            } if (jsonBody[0].status == '3') {
+                console.log('Match ID: ' + jsonBody[0].Match);
+               request({
+                   url: 'http://localhost:5000/api/getmatchplayerdetails/' + jsonBody[0].Match
+               }, function(error, response, body){
+                   var matchBody = JSON.parse(body);
+                   //console.log(matchBody);
+                   if(error){
+                       client.say(ircChannel, errorMsg)
+                   } else {
+                       for (var i = 0; i < matchBody.length; i++){
+                        request({
+                            url: 'http://localhost:5000/api/getplayer/' + matchBody[i].playerName
+                        }, function(error, response, body){
+                            if(error){
+                                client.say(ircChannel, errorMsg)
+                            } else {
+                                try {
+                                    var playerBody = JSON.parse(body);
+                                    var elo = Math.round(playerBody[0].Rank_Stat);
+                                    client.say(ircChannel, playerBody[0].Name + " - ELO:" + elo);
+                                }
+                                catch(e) {
+                                    client.say(ircChannel, e);
+                                }
+                            }
+                        });
+                       } // End 'for' statement
+                   }
+               });
             }
         });
     }
